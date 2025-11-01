51b0384aaee154cc777ee28e62c6858d5565d789
yochiai01 <yui.10feet@gmail.com>
2025-11-01 11:11:30 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,5 +0,0 @@
-const eventBtn = document.getElementById('btn');
-eventBtn.addEventListener('click',()=>{
-    const h2Text = document.getElementById('text');
-    h2Text.textContent = "ボタンをクリックしました";
-})
